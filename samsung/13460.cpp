7378bed737c120ed872aa5aaa5a0340689466d74
silverwest8 <silverwest8@gmail.com>
2023-08-01 13:14:43 +0900

    ✅ 삼성 SW 역량 테스트 기출 문제

---
+++ samsung/13460.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// 2023.7.26 14:00 ~
+
+int main(int argc, const char** argv) {
+    std::cout << "Hello\n" << std::endl;
+    int N, M;
+    cin >> N >> M;
+    cout << N << "\n" << M;
+    return 0;
+}
