bc19c495bd2315c6918e7efc201e4d306b5c6026
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-09-13 22:52:01 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -5,6 +5,31 @@
 
 using namespace std;
 
+bool cmp(const string &a, const string &b) { //string으로 비교 수행
+	return a + b > b + a;
+}
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	vector<string> tmp;
+    
+	for (int i = 0; i < numbers.size(); i++){
+        tmp.push_back(to_string(numbers[i]));
+    }
+    
+	sort(tmp.begin(), tmp.end(), cmp);
+    
+	for (int i = 0; i < tmp.size(); i++) {
+        answer += tmp[i]; 
+    }
+    
+	if (answer[0] == '0') {
+        answer = "0";
+	}
+    
+	return answer;
+}
+
 /*
 bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
     return a.second > b.second;
@@ -73,28 +98,3 @@ string solution(vector<int> numbers) {
     return answer;
 }
 */
-
-bool cmp(const string &a, const string &b) { //string으로 비교 수행
-	return a + b > b + a;
-}
-
-string solution(vector<int> numbers) { //가장 큰 수 - success
-	string answer = "";
-	vector<string> tmp;
-    
-	for (int i = 0; i < numbers.size(); i++){
-        tmp.push_back(to_string(numbers[i]));
-    }
-    
-	sort(tmp.begin(), tmp.end(), cmp); //cmp 함수 기준으로 정렬 수행
-    
-	for (int i = 0; i < tmp.size(); i++) {
-        answer += tmp[i]; 
-    }
-    
-	if (answer[0] == '0') {
-        return "0"; //answer[0]가 0이면 가장 큰 수가 0이라는 뜻이므로 0리턴
-    }
-    
-	return answer;
-}
