e941e103fd7a305b8f124645690a8069e9186124
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-09-13 21:36:08 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
+    return a.second > b.second;
+}
+
+string solution(vector<int> numbers) {    
+
+    string answer = "";
+    std::vector<std::string> number;
+    for (int i = 0; i < numbers.size(); i++) {
+        number.emplace_back(std::to_string(numbers[i]));
+    }
+    while(!number.empty()) {
+        int max = -1;
+        std::vector<pair<int, int>> temp;
+        for (int i = 0; i < number.size(); i++) {
+            if (max < number[i][0] - '0') {
+                max = number[i][0] - '0';
+            }
+        }
+        for (int i = 0; i < number.size(); i++) {
+            if (number[i][0] - '0' == max) {
+                std::string cut = "";
+                cut = number[i].substr(1, number[i].size());
+                if (cut == "") cut = std::to_string(max);
+                // std::cout << "cut == " << cut << "\n";
+                temp.emplace_back(std::make_pair(std::stoi(cut), i));
+            }
+        }
+        
+        
+        std::sort(temp.begin(), temp.end(), std::greater<>());
+        
+        // for (int i = 0; i < temp.size(); i++) {
+        //     std::cout << temp[i].first << " "<< temp[i].second << "\n";
+        // }
+        // std::cout << "\n";
+        
+        
+        for (int i = 0; i < temp.size(); i++) {
+            int idx = temp[i].second;
+            // std::cout << "+= " << number[idx] << "\n";
+            answer += number[idx];
+            // number.erase(std::remove(number.begin(), number.end(), number[idx]));
+            // number.erase(number.begin()+idx);
+        }
+        
+        std::sort(temp.begin(), temp.end(), cmp);
+        // for (int i = 0; i < temp.size(); i++) {
+        //     std::cout << temp[i].first << " "<< temp[i].second << "\n";
+        // }
+        // std::cout << "\n";
+        
+        for (int i = 0; i < temp.size(); i++) {
+            int idx = temp[i].second;
+            number.erase(number.begin()+idx);
+        }
+        // std::cout << "answer == " << answer << "\n\n";
+        
+        // for (int i = 0; i < number.size(); i++) {
+        //     std::cout << number[i] << " ";
+        // }
+        // std::cout << "\n";
+        
+    }
+    return answer;
+}
+*/
+
+bool cmp(const string &a, const string &b) { //string으로 비교 수행
+	return a + b > b + a;
+}
+
+string solution(vector<int> numbers) { //가장 큰 수 - success
+	string answer = "";
+	vector<string> tmp;
+    
+	for (int i = 0; i < numbers.size(); i++){
+        tmp.push_back(to_string(numbers[i]));
+    }
+    
+	sort(tmp.begin(), tmp.end(), cmp); //cmp 함수 기준으로 정렬 수행
+    
+	for (int i = 0; i < tmp.size(); i++) {
+        answer += tmp[i]; 
+    }
+    
+	if (answer[0] == '0') {
+        return "0"; //answer[0]가 0이면 가장 큰 수가 0이라는 뜻이므로 0리턴
+    }
+    
+	return answer;
+}
