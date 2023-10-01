b3a3fbd79316a89ee3b1d40e3e904ce04649af80
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-10-02 02:21:41 +0900

    [unrated] Title: 귤 고르기, Time: 223.88 ms, Memory: 45 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -5,68 +5,32 @@
 
 using namespace std;
 
-int K;
-int answer = 10000000;
 std::vector<int> v(10000000+1, 0);
-std::vector<bool> visited(10000000+1, false);
-std::vector<int> v2(0);
-
-void dfs(int n, int sum) {
-    if (sum >= K) {
-        // int i = 0;
-        // while(i < v2.size()) {
-        //     if (v2[i]) {
-        //         std::cout << v2[i] << " ";
-        //     } else {
-        //         break;
-        //     }
-        //     i++;
-        // }
-        // std::cout << "\n";
-        if (answer > n) answer = n;
-        return;
-    }
-    int i = 0;
-    while(v[i] != 0 && !visited[i]) {
-        visited[i] = true;
-        v2.emplace_back(v[i]);
-        dfs(n+1, sum+v[i]);
-        v2.pop_back();
-        visited[i] = false;
-        i++;
-    }
-}
 
 int solution(int k, vector<int> tangerine) {
-    K = k;
-    // std::vector<std::vector<int>> v;
-    // std::sort(tangerine.begin(), tangerine.end());
-    // int before = 0;
+    int answer = 0;
     for (int i = 0; i < tangerine.size(); i++) {
         v[tangerine[i]]++;
-        // before = tangerine[i];
-        // v.emplace_back(std::vector<int>());
     }
     std::sort(v.begin(), v.end(), std::greater<>());
     
-    int j = 0;
-    while(true) {
-        if (v[j]) {
-            std::cout << v[j] << " ";
-        } else {
-            break;
-        }
-        j++;
-    }
-    std::cout << "\n";
-    // dfs(0, 0);
+    // int j = 0;
+    // while(true) {
+    //     if (v[j]) {
+    //         std::cout << v[j] << " ";
+    //     } else {
+    //         break;
+    //     }
+    //     j++;
+    // }
+    // std::cout << "\n";
     
     int i = 0;
     int sum = 0;
     while(true) {
         if (v[i] != 0) {
             sum+=v[i];
-            if (sum >= K) {
+            if (sum >= k) {
                 answer = i+1;
                 break;
             }
@@ -75,7 +39,6 @@ int solution(int k, vector<int> tangerine) {
         }
         i++;
     }
-    std::cout << "\n";
     
     return answer;
 }
