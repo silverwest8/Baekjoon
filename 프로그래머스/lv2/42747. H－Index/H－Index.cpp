b51048c54fbb4d3c9773f5efddd00bdf891b3789
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-10-06 13:32:01 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -6,28 +6,26 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    // 논문 n편
-    // h번 이상 인용된 논문이 h편 이상이고
-    // h번 이하 인용된 논문이 나머지
-    // h의 최대값
+    
     int answer = 0;
     
-    int n = citations.size();
     std::sort(citations.begin(), citations.end(), std::greater<>());
-    // 0 1 3 5 6
-    // 6 5 3 1 0
-    // 0 0 0
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
     
-    // 6 6 6 6 6
+    return citations.size();
+    /*
+    
+    std::sort(citations.begin(), citations.end(), std::greater<>());
+    
     for (int h = citations[0]; h >= 0; h--) {
-        // std::cout<< h << "\n";
         int count = 0;
-        int count2 = 0;
         for (int i = 0; i < citations.size(); i++) {
             if (citations[i] >= h) {
                 count++;
-            } else {
-                count2++;
             }
             if (count >= h) {
                 answer = h;
@@ -36,5 +34,5 @@ int solution(vector<int> citations) {
         }
     }
     return answer;
-    
+    */
 }
