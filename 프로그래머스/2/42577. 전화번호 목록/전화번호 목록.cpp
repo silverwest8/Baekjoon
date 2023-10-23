f8e61e12314073829af2ff19c35faa9383f1e333
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-10-24 03:45:00 +0900

    [level 2] Title: 전화번호 목록, Time: 72.10 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -7,9 +7,10 @@ using namespace std;
 bool solution(vector<string> phone_book) {
     bool answer = true;
     std::sort(phone_book.begin(), phone_book.end());
-    // 접두어 있으면 false
+
     for (int i = 0; i < phone_book.size()-1; i++) {
         int len = std::min(phone_book[i].size(), phone_book[i+1].size());
+        
         for (int j = 0; j < len; j++) {
             if (phone_book[i][j] != phone_book[i+1][j]) {
                 break;
