7284754b4a147aa8846f6034c18e50790de53203
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-10-26 03:25:24 +0900

    [level 3] Title: 입국심사, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -14,14 +14,14 @@ long long solution(int n, vector<int> times) {
     while(left <= right) {
         long long mid = (left+right)/2;
         long long count = 0;
-        for (int i = 0; i < times.size(); i++) {
-            count += mid/times[i];
+        for (int i=0; i < times.size(); i++) {
+            count += mid / times[i];
         }
-        if (count >= n) {
+        if (count < n) {
+            left = mid+1;
+        } else {
             answer = mid;
             right = mid-1;
-        } else {
-            left = mid+1;
         }
     }
     return answer;
