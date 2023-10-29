2df6db6895440b2f47748d7c7ae10a2d415dca14
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-10-30 03:51:37 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    if (num % 2) {
+        return "Odd";
+    } else {
+        return "Even";
+    }
+}
