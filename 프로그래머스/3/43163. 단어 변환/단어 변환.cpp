83a6a0d8f8d690eaaa7d1fd03e6f55128f998225
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-11-04 03:35:06 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -9,7 +9,6 @@ std::vector<bool> visited;
 int min = 51;
 
 bool compare(std::string a, std::string b) {
-    // std::cout << a << " ----- " << b << "\n";
     int count = 0;
     for (int i = 0; i < a.size(); i++) {
         if (a[i] != b[i]) {
@@ -37,15 +36,7 @@ bool test() {
 }
 
 void DFS(int n) {
-    // for (int i = 0; i < v.size(); i++) {
-    //     std::cout << v[i] << " ";
-    // }
-    // std::cout << "\n";
-    if (n >= min) {
-        return;
-    }
     if (test()) {
-        // std::cout << "here " << n << "\n";
         min = std::min(min, n);
         return;
     }
@@ -65,28 +56,13 @@ void DFS(int n) {
 int solution(std::string begin, std::string target, std::vector<std::string> words) {
     b = begin;
     t = target;
-    int flag = 0;
     for (int i = 0; i < words.size(); i++) {
         word.emplace_back(words[i]);
         visited.emplace_back(false);
-        if (target == words[i]) flag = 1;
     }
-    
-    if (!flag) return 0;
 
-    
-//     std::cout << begin << " " << target << "\n";
-    
-//     for (int i = 0; i < word.size(); i++) {
-//         std::cout << word[i] << " ";
-//     }
-//     std::cout << "\n";
-    
     int answer = 0;
-    
     DFS(0);
-    
     if (min != 51) answer = min;
-    
     return answer;
 }
