377f4dd5c3d618b933ae4aba5cd9e3789d635213
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-11-20 00:37:48 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+std::string solution(std::string number, int k) {
+    std::string answer = "";
+    int n = number.size() - k;
+    int start = 0;
+    for(int i = 0; i < n; i++) {
+        char max = number[start];
+        int maxIdx = start;
+        for(int j = start; j <= i+k; j++) {
+            if(max < number[j]) {
+                max = number[j];
+                maxIdx = j;
+            }
+        }
+        start = maxIdx + 1;
+        answer += max;
+    }
+    
+    
+    return answer;
+}
