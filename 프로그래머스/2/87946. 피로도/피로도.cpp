a9a7ca4d9648e2737ed6b0fa3ff8bfb655e7950a
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2023-11-22 03:38:07 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,26 +1,19 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
-int N;
 std::vector<int> v;
 std::vector<bool> visited;
 int max = 0;
 
 void dfs(int n, int now, std::vector<std::vector<int>> dungeons) {
-    // std::cout << n << " " << now << "\n";
     if (n > max) {
         max = n;
-        
     }
     for (int i = 0; i < dungeons.size(); i++) {
-        if (!visited[i]) {
+        if (!visited[i] && dungeons[i][0] <= now) {
             visited[i] = true;
             v.emplace_back(i);
-            if (dungeons[i][0] <= now) {
-                // std::cout << "add " << i << "\n";
-                dfs(n+1, now-dungeons[i][1], dungeons);
-            }
+            dfs(n+1, now-dungeons[i][1], dungeons);
             v.pop_back();
             visited[i] = false;
         }
@@ -29,7 +22,6 @@ void dfs(int n, int now, std::vector<std::vector<int>> dungeons) {
 
 int solution(int k, std::vector<std::vector<int>> dungeons) {
     int answer = -1;
-    N = dungeons.size();
     for (int i = 0; i < dungeons.size(); i++) {
         visited.emplace_back(false);
     }
