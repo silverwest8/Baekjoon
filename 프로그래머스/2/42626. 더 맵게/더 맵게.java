122b496fbaf099e7e19ee22cd0bfcddcde249ebd
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2024-08-11 17:47:12 +0900

    [level 2] Title: 더 맵게, Time: 1875.40 ms, Memory: 154 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.*;;
+
+public class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        List<Integer> list = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(list);
+        
+        Integer minScoville = queue.poll();
+        while (minScoville < K) {
+            Integer minScoville2 = queue.poll();
+            if (minScoville2 == null) {
+                return -1;
+            }
+            int newScoville = minScoville + minScoville2 * 2;
+            queue.offer(newScoville);
+            minScoville = queue.poll();
+            answer++;
+        }
+        return answer;
+    }
+}
