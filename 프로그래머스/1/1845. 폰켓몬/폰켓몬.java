ffaa340631ed016b482546be55ea19dfd93a3697
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2024-08-11 15:49:55 +0900

    [level 1] Title: 폰켓몬, Time: 9.76 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,6 @@
-import java.util.*;
-import java.util.stream.*;;
-
 class Solution {
-  public int solution(int[] nums) {
-      
-      List<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toList());
-      
-      // Use a Set to find unique elements
-      Set<Integer> uniqueNumbers = new HashSet<>(numbers);
-      
-      // Calculate the maximum number of unique elements you can pick
-      int maxUnique = Math.min(uniqueNumbers.size(), nums.length / 2);
-      
-      return maxUnique;
-
-     
-  }
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
 }
