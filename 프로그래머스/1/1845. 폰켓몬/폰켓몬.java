268facd45c759201c3884f31ee299f9ff49c40a8
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2025-01-12 15:28:16 +0900

    [level 1] Title: 폰켓몬, Time: 8.99 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,6 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+        int num = nums.length / 2;
+        
+        HashSet<Integer> hashSet = new HashSet<>(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        
+        answer = hashSet.size() < num ? hashSet.size() : num;
+        
         return answer;
     }
 }
