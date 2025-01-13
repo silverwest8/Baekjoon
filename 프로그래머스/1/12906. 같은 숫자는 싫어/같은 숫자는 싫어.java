7079706b73409740458f3ca03376e0c6cc8df55a
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2025-01-13 23:01:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.43 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,18 +1,35 @@
 import java.util.*;
-import java.util.stream.*;;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[i - 1]) {
-                list.add(arr[i]);
+    public Deque<Integer> solution(int []arr) {
+        int[] answer = {};
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (deque.size() == 0 || deque.peekLast() != arr[i]) {
+                deque.addLast(arr[i]);
             }
         }
-
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+    
+        return deque;
     }
 }
+
+// import java.util.*;
+// import java.util.stream.*;;
+
+// public class Solution {
+//     public int[] solution(int []arr) {
+//         List<Integer> list = new ArrayList<>();
+//         list.add(arr[0]);
+
+//         for (int i = 1; i < arr.length; i++) {
+//             if (arr[i] != arr[i - 1]) {
+//                 list.add(arr[i]);
+//             }
+//         }
+
+//         int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+//         return answer;
+//     }
+// }
