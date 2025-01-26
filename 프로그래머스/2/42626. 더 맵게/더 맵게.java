b2b37a66db1086b5aa9411722117e33418fbd7a3
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2025-01-26 22:59:26 +0900

    [level 2] Title: 더 맵게, Time: 1719.88 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,23 +1,57 @@
 import java.util.*;
-import java.util.stream.*;
 
 public class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        List<Integer> list = Arrays.stream(scoville).boxed().collect(Collectors.toList());
-        PriorityQueue<Integer> queue = new PriorityQueue<>(list);
-        
-        Integer minScoville = queue.poll();
-        while (minScoville < K) {
-            Integer minScoville2 = queue.poll();
-            if (minScoville2 == null) {
-                return -1;
-            }
-            int newScoville = minScoville + minScoville2 * 2;
-            queue.offer(newScoville);
-            minScoville = queue.poll();
-            answer++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num: scoville) {
+            pq.add(num);
         }
+        while(answer != -1) {
+            if (pq.peek() >= K) {
+                break;
+            }
+            if (pq.size() >= 2) {
+                int first = pq.poll();
+                int second = pq.poll();
+                // System.out.println("first : " +first);
+                // System.out.println("second : " +second);
+                if (first < K) {
+                    pq.add(first + second*2);
+                    // System.out.println(scoville[answer+1]);
+                    answer++;
+                }
+            } else {
+                if (pq.peek() < K) {
+                    answer = -1;
+                }
+            }
+        }
+        
         return answer;
     }
 }
+
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Solution {
+//     public int solution(int[] scoville, int K) {
+//         int answer = 0;
+//         List<Integer> list = Arrays.stream(scoville).boxed().collect(Collectors.toList());
+//         PriorityQueue<Integer> queue = new PriorityQueue<>(list);
+        
+//         Integer minScoville = queue.poll();
+//         while (minScoville < K) {
+//             Integer minScoville2 = queue.poll();
+//             if (minScoville2 == null) {
+//                 return -1;
+//             }
+//             int newScoville = minScoville + minScoville2 * 2;
+//             queue.offer(newScoville);
+//             minScoville = queue.poll();
+//             answer++;
+//         }
+//         return answer;
+//     }
+// }
