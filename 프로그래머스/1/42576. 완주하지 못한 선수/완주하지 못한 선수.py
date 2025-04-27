a675ea02577091f623605e0e645d2a13d26417c1
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2025-04-27 21:09:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.00 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,12 @@
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
-    return participant[-1]
-        
+    answer = ''
+    dic = {}
+    for part in participant:
+        dic[part] = dic.get(part, 0) + 1
+    for com in completion:
+        dic[com] -= 1
+    for key in dic:
+        if dic[key] > 0:
+            answer = key
+            break
+    return answer
