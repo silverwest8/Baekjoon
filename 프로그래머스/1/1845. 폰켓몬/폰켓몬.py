5d41964ff1fa05ab8fd4823d5a7dcf8b274ed91c
silverwest8 <76680379+silverwest8@users.noreply.github.com>
2025-05-25 20:05:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,2 @@
 def solution(nums):
-    answer = len(nums)/2
-    arr = {}
-    for num in nums:
-        if num in arr:
-            arr[num] = arr[num]+1
-        else:
-            arr[num] = 0
-    if len(arr) < len(nums)/2:
-        answer = len(arr)
-    return answer
+    return min(len(nums)/2, len(set(nums)))
